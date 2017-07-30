74ef3c625f94c461d10b1fb1dcaa191897702ecb
kuppilisatishkumar <kuppilisatishkumar1@gmail.com>
2017-07-30 15:52:50 +0530

    implemented PATCH

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var mongoose = require("mongoose");
+//mongoose.Promise = global.Promise;
 
 var bodyParser = require('body-parser');
 
@@ -29,6 +30,9 @@ bookRouter.route('/Books')
         var query = {};
         if(req.query.genre){ //invalid queries will be avoided with IF condition
             query.genre = req.query.genre;
+        }
+        if(req.query.author){ //invalid queries will be avoided with IF condition
+            query.author = req.query.author;
         }
         Book.find(query,function(err,books){
             if(err)
@@ -68,8 +72,25 @@ bookRouter.route('/Books/:bookId') //retrieving by Id
         req.book.author = req.body.author;
         req.book.genre = req.body.genre;
         req.book.read = req.body.read;
-        req.book.save();
-        res.json(req.book);
+        req.book.save(function(err){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(req.book);
+        });
+    })
+    .patch(function(req,res){
+        if(req.body._id)
+            delete req.body._id;
+        for(var p in req.body){
+            req.book[p] = req.body[p];
+        }
+        req.book.save(function(err){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(req.book);
+        });
     });
 
 app.use('/api',bookRouter);
